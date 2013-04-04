a85ae260e92eaee5c1b0aef8ba503c91b63612ee
Nathan Hawes <obitum@yahoo.com>
2013-04-04 20:20:51 +0100

    Fixed for Ruby 1.9.3 p392

--- lib/cheddargetter_client_ruby.rb
+++ lib/cheddargetter_client_ruby.rb
@@ -1,5 +1,7 @@
 require 'httparty'
 require 'cgi' unless Object.const_defined?("CGI")
+require 'rexml/document' unless Object.const_defined?("REXML") && REXML.const_defined?("Document")
+
 
 module CheddarGetter
   autoload :Client, "cheddar_getter/client"
