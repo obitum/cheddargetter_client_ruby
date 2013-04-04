75e9889e2eed9d9ffb19d75a16f0ec3318d8565e
Nathan Hawes <obitum@yahoo.com>
2013-04-04 20:21:11 +0100

    remove newline

--- lib/cheddargetter_client_ruby.rb
+++ lib/cheddargetter_client_ruby.rb
@@ -2,7 +2,6 @@ require 'httparty'
 require 'cgi' unless Object.const_defined?("CGI")
 require 'rexml/document' unless Object.const_defined?("REXML") && REXML.const_defined?("Document")
 
-
 module CheddarGetter
   autoload :Client, "cheddar_getter/client"
   autoload :ClientException, "cheddar_getter/client_exception"
